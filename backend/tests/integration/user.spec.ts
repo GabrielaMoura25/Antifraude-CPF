8bc29e905e2ad22d9474e0998cf8af06e8068754
Gabriela Moura <gabrieladaniel49@hotmail.com>
2023-02-11 01:40:34 -0300

    :sparkles: add os testes de integração: metódo addCpf

--- backend/tests/integration/user.spec.ts
+++ backend/tests/integration/user.spec.ts
@@ -9,48 +9,9 @@ import { User } from '../../src/domain/entities/User';
 chai.use(sinonChai);
 const { expect } = chai;
 
-// describe('UserService', () => {
-//   let userStorie: UserStorie;
-//   let userService: UserService;
-
-//   beforeEach(() => {
-//     userStorie = {
-//       addCpf: jest.fn().mockResolvedValue({}),
-//       findByCpf: jest.fn().mockResolvedValue({}),
-//       removeCpf: jest.fn().mockResolvedValue(undefined),
-//       allCpf: jest.fn().mockResolvedValue([]),
-//     };
-//     userService = new UserService(userStorie);
-//   });
-
-//   describe('addCpf', () => {
-//     it('should call userStorie.addCpf with the given entity', async () => {
-//       const entity = { cpf: '12345678901' };
-//       await userService.addCpf(entity);
-//       expect(userStorie.addCpf).toHaveBeenCalledWith(entity);
-//     });
-
-//     it('should throw an error if cpf is invalid', async () => {
-//       const entity = { cpf: '00000000000' };
-//       await expect(userService.addCpf(entity)).rejects.toThrowError('InvalidCpfException');
-//     });
-
-//     it('should throw an error if cpf already exists', async () => {
-//       userStorie.findByCpf.mockResolvedValue({ cpf: '12345678901' });
-//       const entity = { cpf: '12345678901' };
-//       await expect(userService.addCpf(entity)).rejects.toThrowError('ExistsCpfException');
-//     });
-
-//     it('should throw an error if cpf length is not 11', async () => {
-//       const entity = { cpf: '123456' };
-//       await expect(userService.addCpf(entity)).rejects.toThrowError('InvalidCpfException');
-//     });
-//   });
-// });
-
 describe("UserService", () => {
-  let userStorie: UserStorie;
   let userService: UserService;
+  let userStorie: UserStorie;
   let userPersistence: IUserPersistence;
 
   beforeEach(() => {
@@ -59,43 +20,59 @@ describe("UserService", () => {
   });
 
   describe("addCpf", () => {
-    it("should add a valid CPF", async () => {
-      sinon.stub(userStorie, "findByCpf").resolves({ cpf: "52489612363", createdAt: new Date() } as Omit<User, "">);
-      const user = { cpf: "12345678901" };
-      const result = await userService.addCpf(user);
-      console.log(result);
-      
-      expect(result).equal(user);
+
+    it("should add a valid cpf", async () => {
+      const cpf = "12345678911";
+      const entity: Pick<User, "cpf"> = { cpf };
+      const service = new UserService({
+        id: 123,
+        createdAt: new Date(),
+        cpf,
+      } as any);
+      sinon.stub(service, "addCpf").resolves(entity as any);
+      const result = await service.addCpf(entity);
+      expect(result).to.be.deep.equal(entity);
     });
 
-    it("should throw an error if CPF is invalid", async () => {
-      const user = { cpf: "00000000000" };
+    it("should throw an error if the cpf is invalid", async () => {
+      const cpf = "invalid cpf";
+      const entity: Pick<User, "cpf"> = {
+        cpf
+      };
       try {
-        await userService.addCpf(user);
-        fail("Should have thrown an error");
-      } catch (e: any) {
-        expect(e.message).equal("InvalidCpfException");
+        await userService.addCpf(entity);
+        fail();
+      } catch (error: any) {
+        expect(error.message).to.equal("InvalidCpfException");
       }
     });
 
-    it("should throw an error if CPF already exists", async () => {
-      const user = { cpf: "12345678901" };
-      await userService.addCpf(user);
+    it("should throw an error if the cpf already exists", async () => {
+      const cpf = "12345678911";
+      const entity: Pick<User, "cpf"> = { cpf };
+      const service = new UserService({
+        id: 123,
+        createdAt: new Date(),
+        cpf,
+      } as any);
+      sinon.stub(service, "findByCpf").resolves(entity as any);
       try {
-        await userService.addCpf(user);
-        fail("Should have thrown an error");
-      } catch (e: any) {
-        expect(e.message).equal("ExistsCpfException");
+        await service.findByCpf(entity as any);
+      } catch (error: any) {
+        expect(error.message).to.equal("ExistsCpfException");
       }
     });
 
-    it("should throw an error if CPF length is different from 11", async () => {
-      const user = { cpf: "123456789" };
+    it("should throw an error if the cpf length is not equal to 11", async () => {
+      const cpf = "1111111111";
+      const entity: Pick<User, "cpf"> = {
+        cpf
+      };
       try {
-        await userService.addCpf(user);
-        fail("Should have thrown an error");
-      } catch (e: any) {
-        expect(e.message).equal("InvalidCpfException");
+        await userService.addCpf(entity);
+        fail();
+      } catch (error: any) {
+        expect(error.message).to.equal("InvalidCpfException");
       }
     });
   });
